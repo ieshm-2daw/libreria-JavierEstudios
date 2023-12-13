7837c245575662a702884cf91b1b8adb21f16c04
JavierEstudios <xjfaravi975@ieshnosmachado.org>
2023-12-13 09:09:47 +0100

    CRUD Completo

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from libreria.views import Libreria, DetalleLibro, NuevoLibro
+from libreria.views import Libreria, DetalleLibro, NuevoLibro, EditarLibro
 
 urlpatterns = [
     path('', Libreria.as_view(), name="Libreria"),
     path('libro/<int:pk>', DetalleLibro.as_view(), name="Libro"),
     path('nuevo_libro', NuevoLibro.as_view(), name="Nuevo_Libro"),
+    path('editar_libro/<int:pk>', EditarLibro.as_view(), name="Editar_Libro"),
+    path('eliminar_libro/<int:pk>', EditarLibro.as_view(), name="Eliminar_Libro"),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from libreria.models import Libro
 
 from .forms import Formulario_Libro
@@ -18,4 +18,15 @@ class NuevoLibro (CreateView):
     model = Libro
     fields = ["titulo","autores","editorial","fechaPublicacion","genero","isbn","resumen","disponibilidad","portada"]
     template_name = 'nuevo_libro.html'
+    success_url = reverse_lazy('Libreria')
+
+class EditarLibro (UpdateView):
+    model = Libro
+    fields = ["titulo","autores","editorial","fechaPublicacion","genero","isbn","resumen","disponibilidad","portada"]
+    template_name = 'editar_libro.html'
+    success_url = reverse_lazy('Libreria')
+
+class EditarLibro (DeleteView):
+    model = Libro
+    template_name = 'eliminar_libro.html'
     success_url = reverse_lazy('Libreria')
